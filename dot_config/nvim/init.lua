abce486b90fd959500873722cab70e569964414a
axc <alexcarbajal710@gmail.com>
2024-06-05 03:09:47 -0700

    add nvim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,3 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd("colorscheme onedark") -- load the 'onedarkpro' theme (plugins/themes.lua)
---
+++ dot_config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    opts = {
+      styles = {
+        -- comments = "italic",
+      },
+    },
+  },
+}
