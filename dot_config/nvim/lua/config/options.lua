f389565d5e6d0b718f10594a2f4845f00de6232d
axc <alexcarbajal710@gmail.com>
2024-06-08 02:28:08 -0700

    add .inputrc

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
 vim.opt.shiftwidth = 4 -- Size of an indent
+-- Change the tab and indent size to 2 when working with lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+  end,
+})
