bd9d035a93c0ed8420f55151905173ca3acf5dfb
axc <alexcarbajal710@gmail.com>
2024-08-31 01:40:26 -0700

    change clangd fallback style

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,47 @@
 return {
   "nvim-lspconfig",
   opts = {
+    -- Parameter names displayed within function calls
     inlay_hints = { enabled = false },
+    servers = {
+      -- Ensure mason installs the server
+      clangd = {
+        keys = {
+          { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        },
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern(
+            "Makefile",
+            "configure.ac",
+            "configure.in",
+            "config.h.in",
+            "meson.build",
+            "meson_options.txt",
+            "build.ninja"
+          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+            fname
+          ) or require("lspconfig.util").find_git_ancestor(fname)
+        end,
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          -- Change the formatting style (LLVM, Google, Chromium, Mozilla, WebKit, Microsoft)
+          -- More info: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
+          "--fallback-style=microsoft",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+        },
+      },
+    },
   },
 }
