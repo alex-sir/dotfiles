1d1f68b01fb49b4a317f39fd9bfaab9b52be982a
axc <alexcarbajal710@gmail.com>
2024-11-19 03:32:40 -0800

    change nvim dashboard to snacks

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+______
+.-"      "-.
+/            \
+_          |              |          _
+( \         |,  .-.  .-.  ,|         / )
+> "=._     | )(__/  \__)( |     _.=" <
+(_/"=._"=._ |/     /\     \| _.="_.="\_)
+"=._ (_     ^^     _)"_.="
+"=\__|IIIIII|__/="
+_.="| \IIIIII/ |"=._
+_     _.="_.="\          /"=._"=._     _
+( \_.="_.="     `--------`     "=._"=._/ )
+> _.="                            "=._ <
+(_/               *a x c               \_)
+ ]],
+      },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
