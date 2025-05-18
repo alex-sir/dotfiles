49498e0b444dd6135e9f56e085b118fe5f9bd061
axc <alexcarbajal710@gmail.com>
2025-05-17 19:11:08 -0700

    remove kup

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        header = [[
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				header = [[
 ______
 .-"      "-.
 /            \
@@ -19,14 +19,14 @@ _     _.="_.="\          /"=._"=._     _
 > _.="                            "=._ <
 (_/               *a x c               \_)
  ]],
-      },
-      sections = {
-        { section = "header" },
-        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        { section = "startup" },
-      },
-    },
-  },
+			},
+			sections = {
+				{ section = "header" },
+				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+	},
 }
